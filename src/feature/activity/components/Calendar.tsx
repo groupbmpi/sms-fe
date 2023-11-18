bd2bfa86898239cd61e0420b6149078c2554c5c0
geraldabrhm <geraldabraham16@gmail.com>
2023-11-18 23:31:37 +0700

    refactor: set calendar custom props

--- src/feature/activity/components/Calendar.tsx
+++ src/feature/activity/components/Calendar.tsx
@@ -1,8 +1,19 @@
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
 import listPlugin from "@fullcalendar/list";
+import { EventSourceInput } from "@fullcalendar/core/index.js";
+import { useState } from "react";
 
-const Calendar = () => {
+const Calendar = ({ events }: { events: EventSourceInput }) => {
+  const [eventList, setEventList] = useState<EventSourceInput>([
+    {
+      title: "Kegiatan 1",
+      start: "2023-11-25T10:00:00",
+      end: "2023-11-25T12:00:00",
+      editable: true,
+    },
+  ]);
+
   return (
     <>
       <FullCalendar
@@ -10,14 +21,7 @@ const Calendar = () => {
         firstDay={1}
         plugins={[dayGridPlugin, listPlugin]}
         initialView="dayGridMonth"
-        events={[
-          {
-            title: "Kegiatan 1",
-            start: "2023-11-25T10:00:00",
-            end: "2023-11-25T12:00:00",
-            editable: true,
-          },
-        ]}
+        events={eventList}
         headerToolbar={{
           left: "prev,next today",
           center: "title",
