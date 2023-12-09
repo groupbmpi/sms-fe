0eebcf47665dfdf3ae4e8b54a1750a33dcf39781
dwikalam <dwiikalam@gmail.com>
2023-12-09 16:17:21 +0700

    feat: add maxPageByRecords, generateQueryString, and generateDateStringIdFormat

--- src/helper/Parser.ts
+++ src/helper/Parser.ts
@@ -4,3 +4,34 @@ export const isDomainBCF = (email: string): boolean => {
     const parsedDomain = email.split("@")[1];
     return parsedDomain === BCFDomain;
 }
+
+export const maxPageByRecords = (totalRecords: number, recordsPerPage: number): number => {
+    if (totalRecords <= 0 || recordsPerPage <= 0) {
+        return 1;
+    }
+
+    return Math.ceil(totalRecords / recordsPerPage);
+}
+
+export const generateQueryString = (params: Record<string, any>): string => {
+    const queryString = Object.keys(params)
+        .filter((key) => params[key] !== '' && params[key] !== undefined && params[key] !== null)
+        .map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(params[key])}`)
+        .join('&');
+
+    if (queryString === '') {
+        return '';
+    }
+
+    return `?${queryString}`;
+};
+
+export const generateDateStringIdFormat = (date: Date): string => {
+    const options: Intl.DateTimeFormatOptions = { 
+        day: 'numeric', 
+        month: 'long', 
+        year: 'numeric'
+    };
+
+    return date.toLocaleDateString('id-ID', options);
+};
