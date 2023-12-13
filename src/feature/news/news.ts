d7d2c57e23325050fa5c2a3f74b122120cc97297
dwikalam <dwiikalam@gmail.com>
2023-12-13 10:18:38 +0700

    feat: news related features

--- src/feature/news/news.ts
+++ src/feature/news/news.ts
@@ -1,5 +1,5 @@
 export { AddNewsForm } from "./components/AddNewsForm";
 
-export type { NewsForm } from "./model/News";
+export type * from "./model/News";
 
-export { NewsRepository } from "./repository/NewsRepository";
+export { NewsRepo } from "./repository/NewsRepo";
