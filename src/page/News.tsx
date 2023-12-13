43061f6427929a763bc125c0ec064bc8a6ac7d0b
dwikalam <dwiikalam@gmail.com>
2023-12-13 10:18:53 +0700

    feat: news CRUD pages

--- src/page/News.tsx
+++ src/page/News.tsx
@@ -7,28 +7,36 @@ import {
   ProtectedRoleComponent,
   Role,
 } from "../feature/auth-and-profile/auth-and-profile";
-import { NewsRepository } from "../feature/news/news";
+import { IAllNewsRetDto, IFormAllNewsResponseData, INewsIdArgDto, INewsOptionsArgDto } from "../feature/news/model/News";
+import { generateDateQueryStringFormat, generateDateStringIdFormat, getEndDateByMonthYear, getNumberFromString, getStartDateByMonthYear, maxPageByRecords } from "../helper/Parser";
+import { ResponseType } from "../feature/response";
+import { NewsRepo } from "../feature/news/repository/NewsRepo";
 import { Loading } from "../core/Loading";
 
 const News = () => {
   const [searchParams] = useSearchParams();
-  // TODO make news type in NewsModel
-  const [news, setNews] = useState<
-    {
-      id: number;
-      title: string;
-      detail: string;
-    }[]
-  >([]);
-  const [isLoading, setIsLoading] = useState(true);
 
-  const [filter, setFilter] = useState({
-    "year-news-filter": "all",
-    "month-news-filter": "all",
-    "institution-news-filter": "all",
+  const [news, setNews] = useState<IAllNewsRetDto>({
+    news: [],
+    totalRecords: 0,
   });
 
-  const maxPage = 5; // TODO remove this hardcoded maxPage
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [filter, setFilter] = useState<{
+    "year-news-filter"?: number,
+    "month-news-filter"?: number,
+    "institution-id-news-filter"?: number,
+    "creator-id-news-filter"?: number,    
+  }>({
+    "year-news-filter": undefined,
+    "month-news-filter": undefined,
+    "institution-id-news-filter": undefined,
+    "creator-id-news-filter": undefined,
+  });
+
+  const maxRecordsPerPage = 10;
+  const maxPage = maxPageByRecords(news.totalRecords, maxRecordsPerPage); // TODO remove this hardcoded maxPage
 
   const currentPage = searchParams.get("page");
   const currentPageNum = currentPage ? parseInt(currentPage) : 1;
@@ -40,45 +48,98 @@ const News = () => {
 
   const handleFormChange = (e: React.ChangeEvent) => {
     const target = e.target as HTMLInputElement;
+
     const { id, value } = target;
-    setFilter({ ...filter, [id]: value });
+
+    setFilter({ 
+      ...filter, 
+      [id]: getNumberFromString(value),
+    });
   };
 
   useEffect(() => {
-    console.log(filter);
-  }, [filter]);
-
-  const handleDelete = () => {
-    // TODO delete news
-  };
-
-  useEffect(() => {
-    // TODO fetch news based on currentPageNum and setNews
     setIsLoading(true);
-    NewsRepository.getInstance()
-      .getAllNews(currentPageNum)
-      .then((res) => {
-        setNews(res.news);
+
+    const newsArgDto: INewsOptionsArgDto = {
+      institutionId: filter["institution-id-news-filter"],
+      creatorId: filter["creator-id-news-filter"],
+      startDateAt: filter["month-news-filter"] !== undefined || filter["year-news-filter"] !== undefined
+        ? generateDateQueryStringFormat(getStartDateByMonthYear(
+            filter["month-news-filter"],
+            filter["year-news-filter"]
+          ))
+        : undefined,
+      endDateAt: filter["month-news-filter"] !== undefined || filter["year-news-filter"] !== undefined
+        ? generateDateQueryStringFormat(getEndDateByMonthYear(
+            filter["month-news-filter"],
+            filter["year-news-filter"]
+          ))
+        : undefined,
+      limit: maxRecordsPerPage,
+      page: currentPageNum,
+    }
+
+    NewsRepo.getInstance().getAllNews(newsArgDto)
+      .then(function (response: ResponseType<IFormAllNewsResponseData>) {
+        const data = response.data;
+
+        const newsDto: IAllNewsRetDto = {
+          news: data.news.map(function (item) {
+            return {
+              ...item,
+              createdAt: new Date(item.createdAt),
+              updatedAt: new Date(item.updatedAt)
+            };
+          }),
+          totalRecords: data.totalRecords,
+        };
+
+        setNews(newsDto);
       })
-      .finally(() => {
+      .finally(function () {
         setIsLoading(false);
       });
-  }, [searchParams]);
+  }, [filter, searchParams]);
+
+  const handleDelete = (newsId: number) => {
+    setIsLoading(true);
+
+    const newsArgDto: INewsIdArgDto = { 
+      id: newsId 
+    }
+
+    NewsRepo.getInstance().deleteNews(newsArgDto)
+      .then(function () {
+        const updatedNews = news.news.filter((item) => item.id !== newsArgDto.id);
+
+        const updatedTotalRecords = news.totalRecords - 1;
+
+        setNews({
+          news: updatedNews,
+          totalRecords: updatedTotalRecords,
+        });
+
+        alert('Berhasil menghapus berita');
+      })
+      .finally(function () {
+        setIsLoading(false);
+      });
+  };
 
   return (
     <Container>
       <div className="d-flex py-2 justify-content-between">
         <div className="d-flex gap-2 justify-content-start">
-          <h3>Berita</h3>
+        <h3>Berita</h3>
           <select
             className="form-control"
             id="year-news-filter"
             aria-label="year-news-filter"
             onChange={handleFormChange}
           >
-            <option value="all">Semua Tahun</option>
-            {generateArray(0, 5).map((item) => (
-              <option value={item} key={item}>
+            <option value="">Semua Tahun</option>
+            {generateArray(0, 5).map((item: number) => (
+              <option value={`${new Date().getFullYear() - item}`} key={item}>
                 {new Date().getFullYear() - item}
               </option>
             ))}
@@ -89,7 +150,7 @@ const News = () => {
             aria-label="month-news-filter"
             onChange={handleFormChange}
           >
-            <option value="all">Semua Bulan</option>
+            <option value="">Semua Bulan</option>
             <option value="1">Januari</option>
             <option value="2">Februari</option>
             <option value="3">Maret</option>
@@ -105,11 +166,11 @@ const News = () => {
           </select>
           <select
             className="form-control"
-            id="institution-news-filter"
-            aria-label="institution-news-filter"
+            id="institution-id-news-filter"
+            aria-label="institution-id-news-filter"
             onChange={handleFormChange}
           >
-            <option value="all">Semua Institusi</option>
+            <option value="">Semua Institusi</option>
             <option value="1">Pemerintah</option>
             <option value="2">Swasta</option>
             <option value="3">Pendidikan</option>
@@ -126,31 +187,26 @@ const News = () => {
           }
         />
       </div>
+
       {isLoading ? (
         <Loading />
-      ) : (
+      )  : (
         <>
-          {news.map((item) => (
+          {news.news.map((item) => (
             <div className="card p-1 my-2" key={item.id}>
               <div className="d-flex flex-row">
                 <img
-                  src="https://picsum.photos/200/300"
+                  src={item.photoLink}
                   className="card-img-top"
                   alt={`image-news-${item.id}`}
-                  style={{
-                    width: "250px",
-                    height: "200px",
-                    borderRadius: "10px",
-                  }}
+                  style={{ width: "250px", height: "200px", borderRadius: "10px" }}
                 />
                 <div className="card-body d-flex flex-column justify-content-around">
                   <h5 className="card-title">{item.title}</h5>
                   <div className="text-body-tertiary fst-italic">
-                    16 November 2023 - John Doe
+                    {generateDateStringIdFormat(item.updatedAt)} - {'John Doe'}
                   </div>
-                  <p className="card-text">
-                    {item.detail.substring(0, 100)}...
-                  </p>
+                  <p className="card-text">{item.detail.substring(0, 100)}</p>
                   <div className="d-flex flex-justify-start gap-1">
                     <Link to={`/news/${item.id}`}>
                       <Button variant="primary">Read More</Button>
@@ -162,7 +218,7 @@ const News = () => {
                           <Link to={`/news/${item.id}/edit`}>
                             <Button variant="secondary">Edit</Button>
                           </Link>
-                          <Button variant="danger" onClick={handleDelete}>
+                          <Button variant="danger" onClick={() => handleDelete(item.id)}>
                             Delete
                           </Button>
                         </div>
@@ -175,6 +231,7 @@ const News = () => {
           ))}
         </>
       )}
+      
       <nav aria-label="news-pagination">
         <ul className="pagination justify-content-center">
           <li
--- src/page/NewsPost.tsx
+++ src/page/NewsPost.tsx
@@ -1,27 +1,58 @@
 import { useState } from "react";
 import { Container } from "react-bootstrap";
-
-import { NewsForm, AddNewsForm } from "../feature/news/news";
-
-const initialNewsForm: NewsForm = {
-  newsTitle: "",
-  newsDescription: "",
-  newsImage: "",
-  newsLink: "",
-};
+import { AddNewsForm, ICreateNewsArgDto, NewsForm, NewsRepo } from "../feature/news/news";
+import { useNavigate } from "react-router-dom";
 
 const NewsPost = () => {
-  const [formValue, setFormValue] = useState<NewsForm>(initialNewsForm);
+  console.log('here0');
+
+  const navigate = useNavigate();
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const [formValue, setFormValue] = useState<NewsForm>({
+    title: "",
+    detail: "",
+    photoLink: "",
+    publicationLink: "",
+  });
 
   const handleFormChange = (e: React.ChangeEvent) => {
+    console.log('here');
+
     const target = e.target as HTMLInputElement;
     const id = target.id;
     const value = target.value;
-    setFormValue({ ...formValue, [id]: value });
+
+    setFormValue({ 
+      ...formValue, 
+      [id]: value 
+    });
   };
 
   const handleSubmit = () => {
-    console.log(formValue);
+    console.log('here');
+
+    setIsLoading(true);
+
+    const newsArgDto: ICreateNewsArgDto = {
+      title: formValue.title,
+      detail: formValue.detail,
+      photoLink: formValue.photoLink,
+      creatorId: 1,
+    };
+
+    console.log(newsArgDto);
+
+    NewsRepo.getInstance().createNews(newsArgDto)
+      .then(function () {
+        alert('Berhasil menambahkan berita');
+
+        navigate('/news');
+      })
+      .finally(function () {
+        setIsLoading(false);
+      });
   };
 
   return (
